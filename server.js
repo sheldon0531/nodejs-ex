cb49cb5a7b1baa59033a2af50fc41374fead7728
Sheldon Chang <sheldon@MBP-Sheldon.local>
2018-02-12 14:08:06 +0800

    test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
@@ -71,7 +71,7 @@ app.get('/', function (req, res) {
       if (err) {
         console.log('Error running count. Message:\n'+err);
       }
-    res.render('index.html', { pageCountMessage : count, dbInfo : dbDetails, arrIdx : arrayIndex, ioArrays : ioArray });
+    res.render('index.html', { pageCountMessage : count, dbInfo : dbDetails});
     });
   } else {
     console.log("DB not exists!");
